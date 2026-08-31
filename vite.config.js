b447122f21bac34d58f0dac99da6283381aade30
Afsana <afsanamirzaei9@gmail.com>
2026-09-01 00:48:43 +0430

    fix: correct react plugin typo in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/RentalCar/', // ⬅️ حتماً نام دقیق ریپازیتوری خود را بین دو اسلش بگذارید
+  base: '/RentalCar/', 
 })
