4ca093ed4d0cb87f7bb0912890583f5488551dab
Abdur Rahman <abdurrahmanaipy@gmail.com>
2026-09-02 22:39:07 +0430

    Modified the footer Links and design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ export const Footer = () => {
             tagline: 'همراه امن سفر شما',
             subtext: 'ارائه‌دهنده خدمات کرایه موتر و پشتیبانی سفر در افغانستان',
             quickLinksTitle: 'لینک‌های سریع',
-            links: ['خانه', 'درباره ما', 'موتر ما', 'خدمات', 'تماس با ما'],
+            links: [{ 'label': 'خانه', 'href': '/' }, { 'label': 'درباره ما', 'href': '/about' }, { 'label': 'موتر ما', 'href': '/cars' }, { 'label': 'خدمات', 'href': '/services' }, { 'label': 'تماس با ما', 'href': '/contact' }],
             contactTitle: 'ارتباط با ما',
             address: 'شیرپور، سرک ۳، خانه نمبر ۷، کابل افغانستان',
             copyright: '© 2026 Kabul Rental Car Company. All Rights Reserved.',
@@ -21,7 +21,7 @@ export const Footer = () => {
             tagline: 'Your Safe Travel Partner',
             subtext: 'Providing car rental services and travel support in Afghanistan',
             quickLinksTitle: 'Quick Links',
-            links: ['Home', 'About Us', 'Our Fleet', 'Services', 'Contact Us'],
+            links: [{ 'label': 'Home', 'href': '/' }, { 'label': 'About Us', 'href': '/about' }, { 'label': 'Our Fleet', 'href': '/cars' }, { 'label': 'Services', 'href': '/services' }, { 'label': 'Contact Us', 'href': '/contact' }],
             contactTitle: 'Contact Us',
             address: 'Shirpur, St 3, House #7, Kabul Afghanistan',
             copyright: '© 2026 Kabul Rental Car Company. All Rights Reserved.',
@@ -29,7 +29,7 @@ export const Footer = () => {
     }[lang];
     return (
         <footer
-            className="w-full bg-white text-black font-yekan pt-12 relative overflow-hidden text-center"
+            className="relative w-full bg-white text-black font-yekan pt-12 relative overflow-hidden text-center"
             dir={isRtl ? 'rtl' : 'ltr'}
         >
             {/* Top Main Section */}
@@ -46,7 +46,7 @@ export const Footer = () => {
                         <h3 className="text-[24px] font-[800] leading-tight text-black">
                             {content.tagline}
                         </h3>
-                        <p className="text-[20px] text-gray-700 font-[400] mt-1 leading-relaxed">
+                        <p className="text-[20px]  font-[400] mt-1 leading-relaxed">
                             {content.subtext}
                         </p>
                     </div>
@@ -54,27 +54,29 @@ export const Footer = () => {
                     {/* Social Icons */}
                     <div className="flex items-center gap-2 mt-2">
                         {/* Instagram */}
-                        <a href="#" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
-                            <svg className="w-4 h-4 fill-black" viewBox="0 0 24 24">
-                                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                        <a href="https://www.instagram.com/krc_rental_car?igsi=azR5d21pamFvMTQ3&utm_source=qr" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.3998 2.66675H21.5998C25.8665 2.66675 29.3332 6.13341 29.3332 10.4001V21.6001C29.3332 23.6511 28.5184 25.6181 27.0681 27.0684C25.6178 28.5187 23.6508 29.3334 21.5998 29.3334H10.3998C6.13317 29.3334 2.6665 25.8667 2.6665 21.6001V10.4001C2.6665 8.34907 3.48126 6.38207 4.93154 4.93179C6.38183 3.48151 8.34883 2.66675 10.3998 2.66675ZM10.1332 5.33341C8.86013 5.33341 7.63923 5.83913 6.73906 6.7393C5.83888 7.63948 5.33317 8.86037 5.33317 10.1334V21.8667C5.33317 24.5201 7.47984 26.6667 10.1332 26.6667H21.8665C23.1395 26.6667 24.3604 26.161 25.2606 25.2609C26.1608 24.3607 26.6665 23.1398 26.6665 21.8667V10.1334C26.6665 7.48008 24.5198 5.33341 21.8665 5.33341H10.1332ZM22.9998 7.33341C23.4419 7.33341 23.8658 7.50901 24.1783 7.82157C24.4909 8.13413 24.6665 8.55805 24.6665 9.00008C24.6665 9.44211 24.4909 9.86603 24.1783 10.1786C23.8658 10.4912 23.4419 10.6667 22.9998 10.6667C22.5578 10.6667 22.1339 10.4912 21.8213 10.1786C21.5088 9.86603 21.3332 9.44211 21.3332 9.00008C21.3332 8.55805 21.5088 8.13413 21.8213 7.82157C22.1339 7.50901 22.5578 7.33341 22.9998 7.33341ZM15.9998 9.33341C17.7679 9.33341 19.4636 10.0358 20.7139 11.286C21.9641 12.5363 22.6665 14.232 22.6665 16.0001C22.6665 17.7682 21.9641 19.4639 20.7139 20.7141C19.4636 21.9644 17.7679 22.6667 15.9998 22.6667C14.2317 22.6667 12.536 21.9644 11.2858 20.7141C10.0355 19.4639 9.33317 17.7682 9.33317 16.0001C9.33317 14.232 10.0355 12.5363 11.2858 11.286C12.536 10.0358 14.2317 9.33341 15.9998 9.33341ZM15.9998 12.0001C14.939 12.0001 13.9216 12.4215 13.1714 13.1717C12.4213 13.9218 11.9998 14.9392 11.9998 16.0001C11.9998 17.0609 12.4213 18.0784 13.1714 18.8285C13.9216 19.5787 14.939 20.0001 15.9998 20.0001C17.0607 20.0001 18.0781 19.5787 18.8283 18.8285C19.5784 18.0784 19.9998 17.0609 19.9998 16.0001C19.9998 14.9392 19.5784 13.9218 18.8283 13.1717C18.0781 12.4215 17.0607 12.0001 15.9998 12.0001Z" fill="black" />
                             </svg>
+
                         </a>
                         {/* Facebook */}
-                        <a href="#" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
-                            <svg className="w-4 h-4 fill-black" viewBox="0 0 24 24">
-                                <path d="M9 8H6v4h3v12h5V12h3.642L18 8h-4V6.333C14 5.374 14.5 5 15.5 5H18V0h-3.808C10.592 0 9 1.583 9 4.615V8z" />
+                        <a href="https://youtube.com/@kabulrentalcar?si=TdSuaomS4boNiJ2z" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M29.213 9.427a3.653 3.653 0 0 0-2.573-2.587C24.36 6.2 16 6.2 16 6.2s-8.36 0-10.64.64A3.653 3.653 0 0 0 2.787 9.427C2.16 11.72 2.16 16 2.16 16s0 4.28.627 6.573a3.653 3.653 0 0 0 2.573 2.587c2.28.64 10.64.64 10.64.64s8.36 0 10.64-.64a3.653 3.653 0 0 0 2.573-2.587C29.84 20.28 29.84 16 29.84 16s0-4.28-.627-6.573Z" stroke="black" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M13.2 11.4v9.2l8-4.6-8-4.6Z" stroke="black" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" fill="none" />
                             </svg>
                         </a>
                         {/* TikTok */}
-                        <a href="#" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
-                            <svg className="w-4 h-4 fill-black" viewBox="0 0 24 24">
-                                <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.82.56-1.31 1.56-1.25 2.56.02.83.42 1.64 1.05 2.18.77.66 1.83.87 2.8.61.93-.24 1.71-.97 2.01-1.89.23-.68.27-1.42.27-2.13V.02z" />
+                        <a href="https://www.tiktok.com/@kabulrentalcars?_r=1&_d=f3ia4168eg8cl9&sec_uid=MS4wLjABAAAAr4qcX1OifzdzIeMJJSl1LHbaKdX15nGqonGee-tjZEh5oZKSk4DMVgvBn5NzDqNr&share_author_id=6777446267845952517&sharer_language=en&source=h5_m&u_code=da62f7c3204h7h&item_author_type=1&utm_source=copy&tt_from=copy&enable_checksum=1&utm_medium=ios&share_link_id=97F2A3ED-B371-417B-AB58-9F65A620DB80&user_id=6777446267845952517&sec_user_id=MS4wLjABAAAAr4qcX1OifzdzIeMJJSl1LHbaKdX15nGqonGee-tjZEh5oZKSk4DMVgvBn5NzDqNr&social_share_type=4&ug_btm=b8727,b0&utm_campaign=client_share&share_app_id=1233" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M22.1335 7.76C21.2223 6.71937 20.7201 5.3832 20.7202 4H16.6002V20.5333C16.5691 21.4283 16.1916 22.2761 15.5473 22.898C14.903 23.5199 14.0423 23.8672 13.1469 23.8667C11.2535 23.8667 9.68021 22.32 9.68021 20.4C9.68021 18.1067 11.8935 16.3867 14.1735 17.0933V12.88C9.57354 12.2667 5.54688 15.84 5.54688 20.4C5.54688 24.84 9.22688 28 13.1335 28C17.3202 28 20.7202 24.6 20.7202 20.4V12.0133C22.3909 13.2131 24.3967 13.8569 26.4535 13.8533V9.73333C26.4535 9.73333 23.9469 9.85333 22.1335 7.76Z" fill="black" />
                             </svg>
                         </a>
                         {/* WhatsApp */}
                         <a href="#" className="w-8 h-8 bg-[#FFD000] rounded-full flex items-center justify-center hover:opacity-80 transition-opacity">
-                            <svg className="w-4 h-4 fill-black" viewBox="0 0 24 24">
-                                <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981z" />
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M25.4001 6.54658C24.1775 5.31205 22.7214 4.3332 21.1167 3.66706C19.5121 3.00092 17.7908 2.66082 16.0534 2.66658C8.7734 2.66658 2.84007 8.59991 2.84007 15.8799C2.84007 18.2132 3.4534 20.4799 4.60007 22.4799L2.7334 29.3332L9.7334 27.4932C11.6667 28.5466 13.8401 29.1066 16.0534 29.1066C23.3334 29.1066 29.2667 23.1732 29.2667 15.8932C29.2667 12.3599 27.8934 9.03991 25.4001 6.54658ZM16.0534 26.8666C14.0801 26.8666 12.1467 26.3332 10.4534 25.3332L10.0534 25.0932L5.8934 26.1866L7.00006 22.1332L6.7334 21.7199C5.6368 19.9693 5.05463 17.9456 5.0534 15.8799C5.0534 9.82658 9.98673 4.89324 16.0401 4.89324C18.9734 4.89324 21.7334 6.03991 23.8001 8.11991C24.8236 9.13839 25.6346 10.35 26.1862 11.6843C26.7378 13.0187 27.019 14.4494 27.0134 15.8932C27.0401 21.9466 22.1067 26.8666 16.0534 26.8666ZM22.0801 18.6532C21.7467 18.4932 20.1201 17.6932 19.8267 17.5732C19.5201 17.4666 19.3067 17.4132 19.0801 17.7332C18.8534 18.0666 18.2267 18.8132 18.0401 19.0266C17.8534 19.2532 17.6534 19.2799 17.3201 19.1066C16.9867 18.9466 15.9201 18.5866 14.6667 17.4666C13.6801 16.5866 13.0267 15.5066 12.8267 15.1732C12.6401 14.8399 12.8001 14.6666 12.9734 14.4932C13.1201 14.3466 13.3067 14.1066 13.4667 13.9199C13.6267 13.7332 13.6934 13.5866 13.8001 13.3732C13.9067 13.1466 13.8534 12.9599 13.7734 12.7999C13.6934 12.6399 13.0267 11.0132 12.7601 10.3466C12.4934 9.70658 12.2134 9.78658 12.0134 9.77324H11.3734C11.1467 9.77324 10.8001 9.85324 10.4934 10.1866C10.2001 10.5199 9.34673 11.3199 9.34673 12.9466C9.34673 14.5732 10.5334 16.1466 10.6934 16.3599C10.8534 16.5866 13.0267 19.9199 16.3334 21.3466C17.1201 21.6932 17.7334 21.8932 18.2134 22.0399C19.0001 22.2932 19.7201 22.2532 20.2934 22.1732C20.9334 22.0799 22.2534 21.3732 22.5201 20.5999C22.8001 19.8266 22.8001 19.1732 22.7067 19.0266C22.6134 18.8799 22.4134 18.8132 22.0801 18.6532Z" fill="black" />
                             </svg>
                         </a>
                     </div>
@@ -86,16 +88,26 @@ export const Footer = () => {
                         {content.quickLinksTitle}
                     </h4>
                     <span className="block w-[65px] h-[4px] rounded-[20px] border-b-4 border-[#FFD000] mb-4"></span>
-                    <ul className="space-y-3 text-[15px] font-[600] text-gray-800">
+                    <ul className="space-y-3 text-[15px] font-[600]">
                         {content.links.map((link, idx) => (
                             <li key={idx}>
-                                <a href="#" className="flex items-center gap-2 hover:text-[#FFD000] transition-colors text-[20px] font-[400]">
+                                <a href={link.href} className="flex items-center gap-2 hover:text-[#FFD000] transition-colors text-[20px] font-[400]">
                                     {/* Chevron flips automatically based on LTR/RTL */}
-                                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.31729 6.12832L6.44571 -9.26403e-05L7.97754 1.53174L2.61504 6.89424L7.97754 12.2567L6.44571 13.7886L0.31729 7.66016C0.114197 7.457 0.000105005 7.1815 0.00010503 6.89424C0.000105055 6.60698 0.114197 6.33148 0.31729 6.12832Z" fill="#F7D102" />
-                                    </svg>
+                                    {
+                                        isRtl ? (
+                                            <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M0.31729 6.12832L6.44571 -9.26403e-05L7.97754 1.53174L2.61504 6.89424L7.97754 12.2567L6.44571 13.7886L0.31729 7.66016C0.114197 7.457 0.000105005 7.1815 0.00010503 6.89424C0.000105055 6.60698 0.114197 6.33148 0.31729 6.12832Z" fill="#F7D102" />
+                                            </svg>
+                                        ) : (
+                                            <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.68271 6.12832L1.55429 -9.26403e-05L0.02246 1.53174L5.38496 6.89424L0.02246 12.2567L1.55429 13.7886L7.68271 7.66016C7.8858 7.457 7.99989 7.1815 7.99989 6.89424C7.99989 6.60698 7.8858 6.33148 7.68271 6.12832Z" fill="#F7D102" />
+                                            </svg>
+                                        )
+                                    }
 
-                                    <span>{link}</span>
+
+
+                                    <span>{link.label}</span>
                                 </a>
                             </li>
                         ))}
@@ -109,7 +121,7 @@ export const Footer = () => {
                     </h4>
                     <span className="block w-[65px] h-[4px] rounded-[20px] border-b-4 border-[#FFD000] mb-4"></span>
 
-                    <div className="space-y-4 text-[20px] font-[400] text-gray-800">
+                    <div className="space-y-4 text-[20px] font-[400]">
                         {/* Address */}
                         <div className="flex items-center gap-3">
                             <svg width="18" height="25" viewBox="0 0 18 25" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -143,17 +155,18 @@ export const Footer = () => {
             </div>
 
             {/* Horizontal Divider */}
-            <div className="max-w-7xl mx-auto px-6 mt-12 mb-4">
+            <div className="w-[1200px] mx-auto h-[2px] mt-12 mb-8">
                 <hr className="border-t border-[#FFD000]" />
             </div>
 
             {/* Copyright Notice */}
-            <div className="text-center text-[13px] text-gray-700 font-[500] mb-8" dir="ltr">
+            <div className="text-center text-[20px] font-[400] mb-[60px]" dir="ltr">
                 {content.copyright}
             </div>
 
             {/* Bottom Wave Accent */}
-            <div className="w-full h-24 bg-[#FFD000] rounded-t-[50%] md:rounded-t-[100%]" />
+            {/* <div className="w-full h-24 bg-[#FFD000] rounded-t-[50%] md:rounded-t-[100%]" /> */}
+            <div className="w-full h-[95px] bg-[#F7D102] rounded-t-full -mt-10 -z-10" />
         </footer>
     )
 }
