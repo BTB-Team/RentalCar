2e7afc85d7d378155921de8f49779f45e7937404
Jalal Sadat <sadatsayedjalal50@gmail.com>
2026-09-06 08:53:39 -0700

    Update the About Us section picture on the Home page

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
 
   return (
     <section
-      className="relative min-h-[70vh] sm:min-h-[75vh] md:min-h-screen bg-cover bg-center bg-no-repeat"
+      className="relative min-h-[70vh] pt-4 sm:min-h-[75vh] md:min-h-screen bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${heroBg})` }}
     >
       {/* Bottom gradient overlay */}
