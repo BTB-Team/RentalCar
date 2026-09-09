1f2e44bef680be3d506d5ebfd2df953d924bbc24
MohammadTaqiShirzad <mohammadtaqishirzad293@gmail.com>
2026-09-09 10:50:16 +0430

    Fix CarHeroSkeleton import

--- src/pages/cars/Cars.jsx
+++ src/pages/cars/Cars.jsx
@@ -4,8 +4,8 @@ import { CarFilter } from "../../components/cars/CarFilter";
 import { CarCard } from "../home/components/CarCard";
 import { CarSkeleton } from "../../components/cars/CarSkeleton";
 import { TravelBanner } from "../../components/common/TravelBanner";
-import CarHeroSkeleton from "../../components/cars/CarHeroSkelton";
-import ServiceCtaSkeleton from "../../components/cars/ServiceCtaSkeleton";
+import { CarHeroSkeleton } from "../../components/cars/CarHeroSkelton";
+import { ServiceCtaSkeleton } from "../../components/cars/ServiceCtaSkeleton";
 
 export const Cars = () => {
   const { t, lang, activeFilter } = useLangStore();
